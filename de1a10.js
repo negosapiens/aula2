33b35d2b5bfb656fcb614ca72b6f41e4d03a7fa3
negosapiens <anderson.fraga@sp.senai.com.br>
2022-03-28 20:04:17 -0300

    de1a10_tabuada

---
+++ de1a10.js
@@ -0,0 +1,7 @@
+// contar de 1 a 10
+
+for (let x = 0; x <= 10 ; x++) {
+
+    console.log(x);
+
+}
---
+++ tabuada.js
@@ -0,0 +1,8 @@
+// Tabuada do 8
+
+
+for (let x = 0; x <= 10 ; x++) {
+
+    console.log(x*8);
+
+}
